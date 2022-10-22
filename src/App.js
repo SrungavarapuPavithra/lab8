e7b982bf0ebeed1999a3eb9dab8d53c5899a368f
SrungavarapuPavithra <96069408+SrungavarapuPavithra@users.noreply.github.com>
2022-10-22 09:33:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import DisplayData from "./components/DisplayData";
+function App() {
+  return (
+    <div>
+      <DisplayData />
+    </div>
+  );
+}
+export default App;
